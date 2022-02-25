527a6a8ef5e450ad13d60b85558b1b73991a1c1a
NicholasDevelop <marcaccini.n@gmail.com>
2022-02-25 15:00:22 +0100

    aggiunto codice js

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const app = new Vue ({
     el: '#app',
     data: {
-
+        newTodo: '',
+        todoList: [
+            {
+                text: 'Fare la spesa',
+                done: false,
+            },
+            {
+                text: 'Buttare la spazzatura',
+                done: false,
+            }
+        ]
     },
     methods: {
-     
+        addTodo: function() {
+            if( this.newTodo === '' ){
+                return;
+            }
+            this.text.push( this.newTodo );
+            this.done.push( false );
+            this.newTodo = '';
+        },
+        deleteTodo: function( i ) {
+            this.todoList.splice(i,1)
+        }
     },
 })
